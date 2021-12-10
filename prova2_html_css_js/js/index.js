d7c674dbe2bdac894d422d23c800841f4cfb5ecd
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2021-12-09 22:59:50 -0300

    prova finalizada

--- prova2_html_css_js/js/index.js
+++ prova2_html_css_js/js/index.js
@@ -1,1 +1,54 @@
-console.log("teste 1")
+const message = ["Usuario/Senha incorretos!", "Campos Obrigatórios"]
+
+
+const start = () => {
+    const button = document.getElementById("enter")
+    button.onclick = enterEvent
+}
+
+const enterEvent = event => {
+
+   const user = catchValueUser()
+   const pass = catchValuePass()
+
+   if (user === "" && pass === ""){
+        
+        printMessage(1)
+   
+   }else if (user === "admin" && pass === "abc123"){
+
+        window.location.href = "index2.html"
+
+        
+   }else{
+        printMessage(0)
+       
+   }
+   
+}
+
+const catchValueUser = () => {
+
+    const input = document.getElementById("user")
+    const aux = input.value
+    return aux;
+}
+
+const catchValuePass = () => {
+
+    const input = document.getElementById("pass")
+    const aux = input.value
+    return aux;
+}
+
+const printMessage = (index) => {
+
+        const div = document.getElementById("loginAns")
+        div.innerHTML = ""
+        const p = document.createElement("p")
+        const content = document.createTextNode(message[index])
+        p.appendChild(content)
+        div.insertAdjacentElement("beforeend", p)
+
+}
+start()
